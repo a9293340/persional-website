a5bbbba0afe4cb3af59134adc3682c6ecae57e8b
Eric <f102041332@gmail.com>
2023-11-01 12:40:43 +0800

    [router]

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/views/index.vue";
-import About from "@/views/About/index.vue";
-import Portfolio from "@/views/Portfolio/index.vue";
-import Contact from "@/views/Contact/index.vue";
+import Home from "../views/index.vue";
+import About from "../views/About/index.vue";
+import Portfolio from "../views/Portfolio/index.vue";
+import Contact from "../views/Contact/index.vue";
 
 const routes = [
 	{
