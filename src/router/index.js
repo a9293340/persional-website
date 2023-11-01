52693115cab8cce69e969c5c68a463aa80d946c9
Eric <f102041332@gmail.com>
2023-11-01 13:07:51 +0800

    [test]2

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/index.vue";
 import About from "../views/About/index.vue";
 import Portfolio from "../views/Portfolio/index.vue";
-import Contact from "../views/Contact/index.vue";
 
 const routes = [
 	{
