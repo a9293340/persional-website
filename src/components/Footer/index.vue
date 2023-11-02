51dc692a6db4713c9ee6dacb351bec3d9f0a3829
Eric <f102041332@gmail.com>
2023-11-02 08:41:27 +0800

    [fixed] about mobile over screen issue

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 
 <template>
-  <div class="footer">{{ $t('copyright.desc') }}</div>
+	<div class="footer">{{ $t("copyright.desc") }}</div>
 </template>
 
 <style lang="scss" scoped>
 .footer {
-  @apply fixed left-1/2 bottom-6 xl:bottom-11 -translate-x-1/2 text-sm xl:text-xl whitespace-nowrap text-cyan-800;
+	@apply fixed left-1/2 bottom-4 xl:bottom-11 -translate-x-1/2 text-sm xl:text-xl whitespace-nowrap text-cyan-800;
 }
 </style>
