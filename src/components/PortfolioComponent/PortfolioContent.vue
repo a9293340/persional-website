ca8c017558b2a0f80e96b229991851809e444884
Eric <f102041332@gmail.com>
2023-11-10 08:51:52 +0800

    [fixed] RWD

--- src/components/PortfolioComponent/PortfolioContent.vue
+++ src/components/PortfolioComponent/PortfolioContent.vue
@@ -1,43 +1,43 @@
 <script setup>
-import { useComponentStore } from '@/stores/useComponent';
-import { storeToRefs } from 'pinia';
+import { useComponentStore } from "@/stores/useComponent";
+import { storeToRefs } from "pinia";
 
 const { i18nFontFamily } = storeToRefs(useComponentStore());
 const mobileTrigger = ref(false);
 const props = defineProps({
-  title: {
-    type: String,
-    default: '',
-  },
-  images: {
-    type: Array,
-    default: () => [],
-  },
-  summary: {
-    type: String,
-    default: '',
-  },
-  feature: {
-    type: String,
-    default: '',
-  },
-  link: {
-    type: Array,
-    default: () => [],
-  },
-  tech: {
-    type: Array,
-    default: () => [],
-  },
+	title: {
+		type: String,
+		default: "",
+	},
+	images: {
+		type: Array,
+		default: () => [],
+	},
+	summary: {
+		type: String,
+		default: "",
+	},
+	feature: {
+		type: String,
+		default: "",
+	},
+	link: {
+		type: Array,
+		default: () => [],
+	},
+	tech: {
+		type: Array,
+		default: () => [],
+	},
 });
 
 const linkTo = () => {
-  mobileTrigger.value = !mobileTrigger.value;
+	mobileTrigger.value = !mobileTrigger.value;
 };
 </script>
 
 <template>
-  <div class="mobile-image" v-if="mobileTrigger && props.images.length">
+	<!-- <div class="mobile-image" v-if="mobileTrigger && props.images.length">
     <div class="carousel-rotation">
       <div class="icon">
         <el-icon :size="20" color="blue" @click="linkTo">
@@ -50,179 +50,232 @@ const linkTo = () => {
         </el-carousel-item>
       </el-carousel>
     </div>
-  </div>
-  <div class="portfolio-content-mobile">
-    <div :class="['title', i18nFontFamily, `title-${i18nFontFamily}`]">{{ props.title }}</div>
-    <div class="h-2/5" v-if="props.images.length">
-      <el-carousel width="100%" height="auto" :interval="5000" @click="linkTo">
-        <el-carousel-item v-for="item in props.images" :key="item" style="height: auto">
-          <img :src="item" alt="" />
-        </el-carousel-item>
-      </el-carousel>
-    </div>
-    <div class="content-zone">
-      <div class="content">
-        <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan4') }}</div>
-        <div v-for="item in props.tech" :key="item">
-          <div :class="['slogan-content', `${i18nFontFamily}-title`]" v-if="item.type === 'title'">
-            {{ item.content }}
-          </div>
-          <div
-            :class="['slogan-content', `${i18nFontFamily}-content`]"
-            v-if="item.type === 'content'"
-          >
-            {{ item.content }}
-          </div>
-        </div>
-      </div>
-      <div class="content">
-        <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan3') }}</div>
-        <div
-          :class="['slogan-content', `${i18nFontFamily}-content`]"
-          v-for="item in props.link"
-          :key="item"
-        >
-          <a
-            :href="item.link"
-            class="cursor-pointer hover:text-blue-800 duration-150"
-            target="_blank"
-          >
-            <p class="w-60">* {{ item.title }} (Click me)</p>
-          </a>
-        </div>
-      </div>
-      <div class="content">
-        <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan1') }}</div>
-        <div :class="['slogan-content', `${i18nFontFamily}-content`]">{{ props.summary }}</div>
-      </div>
-      <div class="content">
-        <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan2') }}</div>
-        <div :class="['slogan-content', `${i18nFontFamily}-content`]">{{ props.feature }}</div>
-      </div>
-    </div>
-  </div>
-  <div class="portfolio-content">
-    <div class="title-content">
-      <div :class="['title', i18nFontFamily, `title-${i18nFontFamily}`]">{{ props.title }}</div>
-      <div class="content">
-        <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan4') }}</div>
-        <div v-for="item in props.tech" :key="item">
-          <div :class="['slogan-content', `${i18nFontFamily}-title`]" v-if="item.type === 'title'">
-            {{ item.content }}
-          </div>
-          <div
-            :class="['slogan-content', `${i18nFontFamily}-content`]"
-            v-if="item.type === 'content'"
-          >
-            {{ item.content }}
-          </div>
-        </div>
-      </div>
-      <div class="content">
-        <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan3') }}</div>
-        <div
-          :class="['slogan-content', `${i18nFontFamily}-content`]"
-          v-for="item in props.link"
-          :key="item"
-        >
-          <a
-            :href="item.link"
-            class="cursor-pointer hover:text-blue-800 duration-150"
-            target="_blank"
-          >
-            <p class="w-full">* {{ item.title }} (Click me)</p>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="portfolio-detail-box">
-      <div v-if="props.images.length">
-        <el-carousel width="100%" height="auto" :interval="5000">
-          <el-carousel-item v-for="item in props.images" :key="item" style="height: auto">
-            <img :src="item" alt="" />
-          </el-carousel-item>
-        </el-carousel>
-      </div>
-      <div class="detail-zone">
-        <div class="content">
-          <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan1') }}</div>
-          <div :class="['slogan-content', `${i18nFontFamily}-content`]">{{ props.summary }}</div>
-        </div>
-        <div class="content">
-          <div :class="['slogan', i18nFontFamily]">{{ $t('portfolio.slogan2') }}</div>
-          <div :class="['slogan-content', `${i18nFontFamily}-content`]">{{ props.feature }}</div>
-        </div>
-      </div>
-    </div>
-  </div>
+  </div> -->
+	<div class="portfolio-content-mobile">
+		<div :class="['title', i18nFontFamily, `title-${i18nFontFamily}`]">
+			{{ props.title }}
+		</div>
+		<div class="h-2/5" v-if="props.images.length">
+			<el-carousel width="100%" height="auto" :interval="5000">
+				<el-carousel-item
+					v-for="item in props.images"
+					:key="item"
+					style="height: auto"
+				>
+					<img :src="item" alt="" />
+				</el-carousel-item>
+			</el-carousel>
+		</div>
+		<div
+			:class="[
+				'content-zone',
+				{
+					'h-full': !props.images.length,
+					'h-short': props.images.lengths,
+				},
+			]"
+		>
+			<div class="content">
+				<div :class="['slogan', i18nFontFamily]">
+					{{ $t("portfolio.slogan4") }}
+				</div>
+				<div v-for="item in props.tech" :key="item">
+					<div
+						:class="['slogan-content', `${i18nFontFamily}-title`]"
+						v-if="item.type === 'title'"
+					>
+						{{ item.content }}
+					</div>
+					<div
+						:class="['slogan-content', `${i18nFontFamily}-content`]"
+						v-if="item.type === 'content'"
+					>
+						{{ item.content }}
+					</div>
+				</div>
+			</div>
+			<div class="content">
+				<div :class="['slogan', i18nFontFamily]">
+					{{ $t("portfolio.slogan3") }}
+				</div>
+				<div
+					:class="['slogan-content', `${i18nFontFamily}-content`]"
+					v-for="item in props.link"
+					:key="item"
+				>
+					<a
+						:href="item.link"
+						class="cursor-pointer hover:text-blue-800 duration-150"
+						target="_blank"
+					>
+						<p class="w-60">* {{ item.title }} (Click me)</p>
+					</a>
+				</div>
+			</div>
+			<div class="content">
+				<div :class="['slogan', i18nFontFamily]">
+					{{ $t("portfolio.slogan1") }}
+				</div>
+				<div :class="['slogan-content', `${i18nFontFamily}-content`]">
+					{{ props.summary }}
+				</div>
+			</div>
+			<div class="content">
+				<div :class="['slogan', i18nFontFamily]">
+					{{ $t("portfolio.slogan2") }}
+				</div>
+				<div :class="['slogan-content', `${i18nFontFamily}-content`]">
+					{{ props.feature }}
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="portfolio-content">
+		<div class="title-content">
+			<div :class="['title', i18nFontFamily, `title-${i18nFontFamily}`]">
+				{{ props.title }}
+			</div>
+			<div class="content">
+				<div :class="['slogan', i18nFontFamily]">
+					{{ $t("portfolio.slogan4") }}
+				</div>
+				<div v-for="item in props.tech" :key="item">
+					<div
+						:class="['slogan-content', `${i18nFontFamily}-title`]"
+						v-if="item.type === 'title'"
+					>
+						{{ item.content }}
+					</div>
+					<div
+						:class="['slogan-content', `${i18nFontFamily}-content`]"
+						v-if="item.type === 'content'"
+					>
+						{{ item.content }}
+					</div>
+				</div>
+			</div>
+			<div class="content">
+				<div :class="['slogan', i18nFontFamily]">
+					{{ $t("portfolio.slogan3") }}
+				</div>
+				<div
+					:class="['slogan-content', `${i18nFontFamily}-content`]"
+					v-for="item in props.link"
+					:key="item"
+				>
+					<a
+						:href="item.link"
+						class="cursor-pointer hover:text-blue-800 duration-150"
+						target="_blank"
+					>
+						<p class="w-full">* {{ item.title }} (Click me)</p>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="portfolio-detail-box">
+			<div v-if="props.images.length">
+				<el-carousel width="100%" height="auto" :interval="5000">
+					<el-carousel-item
+						v-for="item in props.images"
+						:key="item"
+						style="height: auto"
+					>
+						<img :src="item" alt="" />
+					</el-carousel-item>
+				</el-carousel>
+			</div>
+			<div class="detail-zone">
+				<div class="content">
+					<div :class="['slogan', i18nFontFamily]">
+						{{ $t("portfolio.slogan1") }}
+					</div>
+					<div :class="['slogan-content', `${i18nFontFamily}-content`]">
+						{{ props.summary }}
+					</div>
+				</div>
+				<div class="content">
+					<div :class="['slogan', i18nFontFamily]">
+						{{ $t("portfolio.slogan2") }}
+					</div>
+					<div :class="['slogan-content', `${i18nFontFamily}-content`]">
+						{{ props.feature }}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .mobile-image {
-  @apply w-full h-full absolute bg-white z-50;
-  .icon {
-    @apply absolute -left-1 -top-5 cursor-pointer z-80000;
-  }
-  .carousel-rotation {
-    @apply w-full h-full;
-    transform: rotate(90deg) translateY(63%) scale(1.7);
-  }
+	@apply w-full h-full absolute bg-white z-50;
+	.icon {
+		@apply absolute -left-1 -top-5 cursor-pointer z-80000;
+	}
+	.carousel-rotation {
+		@apply w-full h-full;
+		transform: rotate(90deg) translateY(63%) scale(1.7);
+	}
 }
 .portfolio-content {
-  @apply w-full h-full lg:flex lg:flex-row hidden;
-  .title-content {
-    @apply w-full lg:w-5/12 h-1/2 lg:h-full lg:pt-16;
-    .title {
-      @apply lg:text-4xl text-gray-800 mb-14;
-    }
-    .content {
-      @apply flex flex-col w-11/12 mb-5 ml-6;
-      .slogan {
-        @apply lg:text-3xl text-sky-900 mb-8;
-      }
-      .slogan-content {
-        @apply text-lg mb-3 w-full flex flex-row;
-      }
-    }
-    .title-zh {
-      @apply lg:text-5xl;
-    }
-  }
-  .portfolio-detail-box {
-    @apply w-full lg:w-7/12 h-1/2  lg:h-full;
-    .detail-zone {
-      @apply w-full h-2/5 flex flex-row justify-between mt-16;
-      .content {
-        @apply flex flex-col w-[calc(50%-15px)];
-        .slogan {
-          @apply lg:text-3xl text-sky-900 mb-4;
-        }
-        .slogan-content {
-          @apply text-lg mb-3 w-full flex flex-row;
-        }
-      }
-    }
-  }
+	@apply w-full h-full lg:flex lg:flex-row hidden;
+	.title-content {
+		@apply w-full lg:w-5/12 h-1/2 lg:h-full lg:pt-16;
+		.title {
+			@apply lg:text-4xl text-gray-800 mb-14;
+		}
+		.content {
+			@apply flex flex-col w-11/12 mb-5 ml-6;
+			.slogan {
+				@apply lg:text-3xl text-sky-900 mb-8;
+			}
+			.slogan-content {
+				@apply text-lg mb-3 w-full flex flex-row;
+			}
+		}
+		.title-zh {
+			@apply lg:text-5xl;
+		}
+	}
+	.portfolio-detail-box {
+		@apply w-full lg:w-7/12 h-1/2  lg:h-full;
+		.detail-zone {
+			@apply w-full h-2/5 flex flex-row justify-between mt-16;
+			.content {
+				@apply flex flex-col w-[calc(50%-15px)];
+				.slogan {
+					@apply lg:text-3xl text-sky-900 mb-4;
+				}
+				.slogan-content {
+					@apply text-lg mb-3 w-full flex flex-row;
+				}
+			}
+		}
+	}
 }
 .portfolio-content-mobile {
-  @apply w-full h-full flex lg:hidden flex-col m-4 mt-10;
-  .title {
-    @apply text-lg text-gray-800 mb-8;
-  }
-  .title-zh {
-    @apply lg:text-3xl;
-  }
-  .content-zone {
-    @apply w-full h-3/5 overflow-auto scrollbar-thin;
-    .content {
-      @apply flex flex-col w-full mb-4;
-      .slogan {
-        @apply text-lg text-sky-900 mb-4;
-      }
-      .slogan-content {
-        @apply text-lg mb-3 w-full flex flex-row;
-      }
-    }
-  }
+	@apply w-full h-full flex lg:hidden flex-col m-4 mt-10;
+	.title {
+		@apply text-lg text-gray-800 mb-8;
+	}
+	.title-zh {
+		@apply lg:text-3xl;
+	}
+	.content-zone {
+		@apply w-full overflow-y-scroll overflow-x-hidden;
+		.content {
+			@apply flex flex-col w-full mb-4;
+			.slogan {
+				@apply text-lg text-sky-900 mb-4;
+			}
+			.slogan-content {
+				@apply text-lg mb-3 w-full flex flex-row;
+			}
+		}
+	}
+	.h-short {
+		@apply h-[calc(80%)];
+	}
 }
 </style>
--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { SelfIntro } from '@/three/SelfIntroduction/SelfIntro.js';
-import { useComponentStore } from '@/stores/useComponent';
-import { storeToRefs } from 'pinia';
-import { useRouter } from 'vue-router';
-import { init } from '@/assets/utils/init';
+import { SelfIntro } from "@/three/SelfIntroduction/SelfIntro.js";
+import { useComponentStore } from "@/stores/useComponent";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+import { init } from "@/assets/utils/init";
 
 let selfIntro = null;
 const page = ref(0);
@@ -12,173 +12,205 @@ const { i18nFontFamily } = storeToRefs(useComponentStore());
 const router = useRouter();
 
 router.afterEach(async (to, from) => {
-  if (to.fullPath === '/about') await initThree();
+	if (to.fullPath === "/about") await initThree();
 });
 
-const pick = ref('information');
+const pick = ref("information");
 
 const initThree = async () => {
-  selfIntro = await init('#self-introduction', selfIntro, SelfIntro);
+	selfIntro = await init("#self-introduction", selfIntro, SelfIntro);
 };
 
-const pickInfo = type => {
-  pick.value = type;
-  switch (type) {
-    case 'information':
-      selfIntro.reset();
-      page.value = 0;
-      break;
-    case 'interest':
-      selfIntro.setLight();
-      page.value = 1;
-      break;
-    case 'experience':
-      selfIntro.moveCamera();
-      page.value = 2;
-      break;
-    case 'technology':
-      selfIntro.shakeMillennium();
-      page.value = 3;
-      break;
-  }
+const pickInfo = (type) => {
+	pick.value = type;
+	switch (type) {
+		case "information":
+			selfIntro.reset();
+			page.value = 0;
+			break;
+		case "interest":
+			selfIntro.setLight();
+			page.value = 1;
+			break;
+		case "experience":
+			selfIntro.moveCamera();
+			page.value = 2;
+			break;
+		case "technology":
+			selfIntro.shakeMillennium();
+			page.value = 3;
+			break;
+	}
 };
 
 onMounted(async () => {
-  await initThree();
+	await initThree();
 });
 </script>
 
 <template>
-  <div class="self-introduction">
-    <div id="self-introduction"></div>
-    <div class="introduction">
-      <div class="top">
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'information' }]"
-          @click="pickInfo('information')"
-        >
-          {{ $t('about.information') }}
-        </div>
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'interest' }]"
-          @click="pickInfo('interest')"
-        >
-          {{ $t('about.interest') }}
-        </div>
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'experience' }]"
-          @click="pickInfo('experience')"
-        >
-          {{ $t('about.experience') }}
-        </div>
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'technology' }]"
-          @click="pickInfo('technology')"
-        >
-          {{ $t('about.technology') }}
-        </div>
-      </div>
-      <div class="content">
-        <img src="/image/me/5813250-B20231106.jpg" alt="" />
-        <AboutContent v-model:page="page" />
-      </div>
-    </div>
-    <div class="introduction-mobile">
-      <div class="list">
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'information' }]"
-          @click="pickInfo('information')"
-        >
-          {{ $t('about.information') }}
-        </div>
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'interest' }]"
-          @click="pickInfo('interest')"
-        >
-          {{ $t('about.interest') }}
-        </div>
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'experience' }]"
-          @click="pickInfo('experience')"
-        >
-          {{ $t('about.experience') }}
-        </div>
-        <div
-          :class="['header-words', i18nFontFamily, { pick: pick === 'technology' }]"
-          @click="pickInfo('technology')"
-        >
-          {{ $t('about.technology') }}
-        </div>
-      </div>
-      <div class="content">
-        <img src="/image/me/5813250-B20231106.jpg" alt="" />
-        <AboutContent v-model:page="page" />
-      </div>
-    </div>
-  </div>
+	<div class="self-introduction">
+		<div id="self-introduction"></div>
+		<div class="introduction">
+			<div class="top">
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'information' },
+					]"
+					@click="pickInfo('information')"
+				>
+					{{ $t("about.information") }}
+				</div>
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'interest' },
+					]"
+					@click="pickInfo('interest')"
+				>
+					{{ $t("about.interest") }}
+				</div>
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'experience' },
+					]"
+					@click="pickInfo('experience')"
+				>
+					{{ $t("about.experience") }}
+				</div>
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'technology' },
+					]"
+					@click="pickInfo('technology')"
+				>
+					{{ $t("about.technology") }}
+				</div>
+			</div>
+			<div class="content">
+				<img src="/image/me/5813250-B20231106.jpg" alt="" />
+				<AboutContent v-model:page="page" />
+			</div>
+		</div>
+		<div class="introduction-mobile">
+			<div class="list">
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'information' },
+					]"
+					@click="pickInfo('information')"
+				>
+					{{ $t("about.information") }}
+				</div>
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'interest' },
+					]"
+					@click="pickInfo('interest')"
+				>
+					{{ $t("about.interest") }}
+				</div>
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'experience' },
+					]"
+					@click="pickInfo('experience')"
+				>
+					{{ $t("about.experience") }}
+				</div>
+				<div
+					:class="[
+						'header-words',
+						i18nFontFamily,
+						{ pick: pick === 'technology' },
+					]"
+					@click="pickInfo('technology')"
+				>
+					{{ $t("about.technology") }}
+				</div>
+			</div>
+			<div class="content">
+				<img src="/image/me/5813250-B20231106.jpg" alt="" />
+				<AboutContent v-model:page="page" />
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .self-introduction,
 #self-introduction {
-  @apply w-full h-full;
+	@apply w-full h-full;
 }
 
 .self-introduction {
-  @apply relative;
-  .introduction-mobile {
-    @apply lg:hidden flex flex-row-reverse w-full h-3/4 pl-2 bg-slate-200 bg-opacity-20 backdrop-blur-sm left-1/2 -translate-x-1/2 top-32 z-50 fixed;
-    .list {
-      @apply flex flex-col fixed left-4 top-2;
-      .header-words {
-        @apply text-orange-300 mb-2 cursor-pointer duration-150;
-        &:nth-child(1) {
-          @apply text-4xl;
-        }
-        &:nth-child(2) {
-          @apply text-3xl;
-          // font-size: 2rem;
-        }
-        &:nth-child(3) {
-          @apply text-2xl;
-          font-size: 1.75rem;
-        }
-        &:nth-child(4) {
-          @apply text-xl;
-          font-size: 1.5rem;
-        }
-      }
-      .pick {
-        @apply italic text-white;
-      }
-    }
-    .content {
-      @apply w-11/12 h-3/5 mt-10;
-      img {
-        @apply lg:w-60 w-48 mb-6 ml-28 lg:ml-20;
-      }
-    }
-  }
-  .introduction {
-    @apply fixed h-2/3 z-50 m-10 lg:flex flex-row hidden;
-    width: 54%;
-    left: 6%;
-    top: 25%;
-    .top {
-      @apply h-full flex flex-col justify-start items-start;
-      .header-words {
-        @apply text-orange-300 text-3xl mb-10 cursor-pointer duration-150;
-      }
-      .pick {
-        @apply italic text-white scale-125;
-      }
-    }
-    .content {
-      @apply w-11/12 h-full p-8 pl-12;
-      img {
-        @apply lg:w-60 w-48 mb-8 ml-24 lg:ml-20;
-      }
-    }
-  }
+	@apply relative;
+	.introduction-mobile {
+		@apply lg:hidden flex flex-row-reverse w-full h-3/4 pl-0 bg-slate-200 bg-opacity-20 backdrop-blur-sm left-1/2 -translate-x-1/2 top-32 z-50 fixed;
+		.list {
+			@apply flex flex-col fixed left-4 top-2;
+			.header-words {
+				@apply text-orange-300 mb-2 cursor-pointer duration-150;
+				&:nth-child(1) {
+					@apply text-4xl;
+				}
+				&:nth-child(2) {
+					@apply text-3xl;
+					// font-size: 2rem;
+				}
+				&:nth-child(3) {
+					@apply text-2xl;
+					font-size: 1.75rem;
+				}
+				&:nth-child(4) {
+					@apply text-xl;
+					font-size: 1.5rem;
+				}
+			}
+			.pick {
+				@apply italic text-white;
+			}
+		}
+		.content {
+			@apply w-11/12 h-3/5 mt-10;
+			img {
+				@apply lg:w-60 w-40 mb-6 ml-28 lg:ml-20;
+			}
+		}
+	}
+	.introduction {
+		@apply fixed h-2/3 z-50 m-10 lg:flex flex-row hidden;
+		width: 54%;
+		left: 6%;
+		top: 25%;
+		.top {
+			@apply h-full flex flex-col justify-start items-start;
+			.header-words {
+				@apply text-orange-300 text-3xl mb-10 cursor-pointer duration-150;
+			}
+			.pick {
+				@apply italic text-white scale-125;
+			}
+		}
+		.content {
+			@apply w-11/12 h-full p-8 pl-12;
+			img {
+				@apply lg:w-60 w-40 mb-8 ml-24 lg:ml-20;
+			}
+		}
+	}
 }
 </style>
